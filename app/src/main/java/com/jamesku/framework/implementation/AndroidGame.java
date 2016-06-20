fab49862927cba5ec1c441d1c6a0ed0c99384fd3
Tim <tim840815@gmail.com>
2016-06-20 21:58:24 +0800

    123

--- app/src/main/java/com/jamesku/framework/implementation/AndroidGame.java
+++ app/src/main/java/com/jamesku/framework/implementation/AndroidGame.java
@@ -39,8 +39,8 @@ public abstract class AndroidGame extends Activity implements Game {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-        int frameBufferWidth = isPortrait ? 480: 800;
-        int frameBufferHeight = isPortrait ? 800: 480;
+        int frameBufferWidth = isPortrait ? 1080: 1920;
+        int frameBufferHeight = isPortrait ? 1920: 1080;
         Bitmap frameBuffer = Bitmap.createBitmap(frameBufferWidth,
                 frameBufferHeight, Config.RGB_565);
 
