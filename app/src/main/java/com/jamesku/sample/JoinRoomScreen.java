2c3d4753a569b9050a69d26eda4f091e360ad7f0
Tim <tim840815@gmail.com>
2016-06-22 23:55:28 +0800

    Finish Join room UI

--- app/src/main/java/com/jamesku/sample/JoinRoomScreen.java
+++ app/src/main/java/com/jamesku/sample/JoinRoomScreen.java
@@ -39,43 +39,43 @@ public class JoinRoomScreen extends Screen{
             TouchEvent event = (TouchEvent)o;
 
             if(event.type == TouchEvent.TOUCH_DOWN) {
-                    if (inCircle(event, 80, 400, 40)) {
+                    if (inBounds(event, 0, 320, 120, 120)) {
                         IP.append('1');
                         break;
-                    } else if (inCircle(event, 160, 400, 40)) {
+                    } else if (inBounds(event, 120, 320, 120, 120)) {
                         IP.append('2');
                         break;
-                    } else if (inCircle(event, 240, 400, 40)) {
+                    } else if (inBounds(event, 240, 320, 120, 120)) {
                         IP.append('3');
                         break;
-                    } else if (inCircle(event, 80, 500, 40)) {
+                    } else if (inBounds(event, 0, 440, 120, 120)) {
                         IP.append('4');
                         break;
-                    } else if (inCircle(event, 160, 500, 40)) {
+                    } else if (inBounds(event, 120, 440, 120, 120)) {
                         IP.append('5');
                         break;
-                    } else if (inCircle(event, 240, 500, 40)) {
+                    } else if (inBounds(event, 240, 440, 120, 120)) {
                         IP.append('6');
                         break;
-                    } else if (inCircle(event, 80, 600, 40)) {
+                    } else if (inBounds(event, 0, 560, 120, 120)) {
                         IP.append('7');
                         break;
-                    } else if (inCircle(event, 160, 600, 40)) {
+                    } else if (inBounds(event, 120, 560, 120, 120)) {
                         IP.append('8');
                         break;
-                    } else if (inCircle(event, 240, 600, 40)) {
+                    } else if (inBounds(event, 240, 560, 120, 120)) {
                         IP.append('9');
                         break;
-                    } else if (inCircle(event, 80, 700, 40)) {
+                    } else if (inBounds(event, 0, 680, 120, 120)) {
                         IP.append('.');
                         break;
-                    } else if (inCircle(event, 160, 700, 40)) {
+                    } else if (inBounds(event, 120, 680, 120, 120)) {
                         IP.append('0');
                         break;
-                    } else if (inCircle(event, 240, 700, 40)) {
+                    } else if (inBounds(event, 360, 320, 120, 120)) {
                         if (IP.length() != 0)
                             IP.deleteCharAt(IP.length() - 1);
-                    } else if (inCircle(event, 400, 700, 40)) {
+                    } else if (inBounds(event, 360, 440, 120, 360)) {
                         try {
                             socket = new Socket(IP.toString(), 8991);
                             Log.d("INFO", "update: CONNECT TO " + IP.toString());
@@ -97,13 +97,21 @@ public class JoinRoomScreen extends Screen{
         Graphics g = game.getGraphics();
         g.drawARGB(255, 0, 0, 0);
 
-        Paint paint;
+        Paint paint,paint1;
         paint = new Paint();
-        paint.setTextSize(30);
+        paint.setTextSize(50);
         paint.setTextAlign(Paint.Align.CENTER);
         paint.setAntiAlias(true);
         paint.setColor(Color.WHITE);
-        g.drawString(IP.toString(), 100, 50, paint);
+
+        paint1 = new Paint();
+        paint1.setTextSize(70);
+        paint1.setTextAlign(Paint.Align.CENTER);
+        paint1.setAntiAlias(true);
+        paint1.setColor(Color.WHITE);
+        g.drawString("Enter Host IP:", 240, 100, paint1);
+
+        g.drawString(IP.toString(), 240, 200, paint);
 
         //g.drawARGB(255, 127, 127, 127);
         Rect dstRect = new Rect();
@@ -166,11 +174,13 @@ public class JoinRoomScreen extends Screen{
         System.gc();
     }
 
-    private boolean inCircle(TouchEvent event, int x, int y, int radius){
-        int distance2 = (event.x - x) * (event.x - x) + (event.y - y) * (event.y - y);
-        if(distance2 < radius*radius)
+    private boolean inBounds(TouchEvent event, int x, int y, int width,
+                             int height) {
+        if (event.x > x && event.x < x + width - 1 && event.y > y
+                && event.y < y + height - 1)
             return true;
-        return false;
+        else
+            return false;
     }
 
     public Socket getSocket() {
